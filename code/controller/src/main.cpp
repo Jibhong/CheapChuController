1b2cf5f683f60942dbc27098f40a197c845713b4
BuildTools <pisitjibhong@gmail.com>
2026-08-01 14:00:21 +0900

    Slider now work

--- code/controller/src/main.cpp
+++ code/controller/src/main.cpp
@@ -1,16 +1,52 @@
+
+
+
+
+// static void i2cScan() {
+//   Serial.println("Scanning Wire1 I2C bus...");
+//   uint8_t found = 0;
+
+//   for (uint8_t addr = 1; addr < 127; addr++) {
+//     Wire1.beginTransmission(addr);
+//     uint8_t err = Wire1.endTransmission();
+//     if (err == 0) {
+//       Serial.print("  Device found at 0x");
+//       if (addr < 16)
+//         Serial.print("0");
+//       Serial.println(addr, HEX);
+//       found++;
+//     }
+//   }
+
+//   if (found == 0) {
+//     Serial.println("  No I2C devices found!");
+//   }
+//   Serial.println("Scan complete.");
+// }
+
+
 #include <Arduino.h>
 #include <USB.h>
+
+#include <Wire.h>
+extern "C"
+{
+#include "mpr121.h"
+}
 
 // # Custom VID PID so chuniio.dll can find the device
 #define PICO_USB_VID 0x2E8A
 #define PICO_USB_PID 0x000A
+
+#define MPR121_ADDR_A 0x5B
+#define MPR121_ADDR_B 0x5A
 
 // ---------------------------------------------------------------------
 // Packet framing constants
 // ---------------------------------------------------------------------
 static constexpr uint8_t SYNC0 = 0xAA;
 static constexpr uint8_t SYNC1 = 0x55;
-static constexpr size_t  PACKET_SIZE = 44; // 2 sync + 1 seq + 32 slider + 6 air + 2 btn + 1 checksum
+static constexpr size_t PACKET_SIZE = 44; // 2 sync + 1 seq + 32 slider + 6 air + 2 btn + 1 checksum
 
 static uint8_t seq_counter = 0;
 
@@ -20,95 +56,164 @@ static uint8_t air_beams[6] = {0};
 static uint8_t test_btn = 0;
 static uint8_t service_btn = 0;
 
+static void scanReboot()
+{
+  static bool isInit = 0;
+  if (!isInit)
+  {
+    pinMode(16, INPUT_PULLUP);
+    isInit = 1;
+  }
+  if (digitalRead(16) == LOW)
+  {
+    delay(20);
+    if (digitalRead(16) == LOW)
+    {
+      rp2040.reboot();
+    }
+  }
+}
+
+static void initSlider()
+{
+  Wire1.setSDA(26);
+  Wire1.setSCL(27);
+  Wire1.begin();
+
+  if (!mpr121_init(MPR121_ADDR_A))
+  {
+    Serial.println("MPR121 A not found");
+    while (1)
+    {
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+      delay(200);
+
+      if (digitalRead(16) == LOW)
+      {
+        delay(20);
+        if (digitalRead(16) == LOW)
+        {
+          rp2040.reboot();
+        }
+      }
+    }
+  }
+  if (!mpr121_init(MPR121_ADDR_B))
+  {
+    Serial.println("MPR121 B not found");
+    while (1)
+    {
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+      delay(200);
+
+      if (digitalRead(16) == LOW)
+      {
+        delay(20);
+        if (digitalRead(16) == LOW)
+        {
+          rp2040.reboot();
+        }
+      }
+    }
+  }
+}
 
 // fill slider_pressure[0..31] with 0-255 pressure values.
-void scan_slider() {
-    bool ok = digitalRead(16) == LOW;
-    if(ok){
-        digitalWrite(LED_BUILTIN, HIGH);
-        slider_pressure[0] = 255;
-        slider_pressure[1] = 255;
-        slider_pressure[0] = 255;
-    }
-    else{
-        digitalWrite(LED_BUILTIN, LOW);
-        slider_pressure[0] = 0;
-        slider_pressure[1] = 0;
-        slider_pressure[0] = 0;
-    }
+static void scan_slider()
+{
+  // Get the touch status of all 12 pins at once
+  uint16_t touchedRegistersA = mpr121_touched(MPR121_ADDR_A);
+  uint16_t touchedRegistersB = mpr121_touched(MPR121_ADDR_B);
+
+  // Loop through all 12 pins (0 to 11)
+  for (uint8_t i = 0; i < 8; i++)
+    slider_pressure[31-(i*2)] = touchedRegistersA & (1 << i) ? 255 : 0;
+
+  for (uint8_t i = 4; i < 12; i++)
+    slider_pressure[31-((i - 4 + 8)*2)] = touchedRegistersB & (1 << i) ? 255 : 0;
 }
 
 // fill air_beams[0..5] with 0/1.
-void scan_air() {
-    bool ok = digitalRead(16) == LOW;
-    if(ok){
-        digitalWrite(LED_BUILTIN, HIGH);
-        air_beams[0] = 1;
-        air_beams[1] = 1;
-        air_beams[0] = 1;
-    }
-    else{
-        digitalWrite(LED_BUILTIN, LOW);
-        air_beams[0] = 0;
-        air_beams[1] = 0;
-        air_beams[0] = 0;
-    }
-    
+void scan_air()
+{
+  bool ok = digitalRead(16) == LOW;
+  if (ok)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    air_beams[0] = 1;
+    air_beams[1] = 1;
+    air_beams[0] = 1;
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    air_beams[0] = 0;
+    air_beams[1] = 0;
+    air_beams[0] = 0;
+  }
 }
 
-void scan_buttons() {
-    
+void scan_buttons()
+{
 }
 
 // # Build and send one packet over USB serial.
-void send_packet() {
-    uint8_t pkt[PACKET_SIZE];
-    size_t i = 0;
+void send_packet()
+{
+  uint8_t pkt[PACKET_SIZE];
+  size_t i = 0;
 
-    pkt[i++] = SYNC0;
-    pkt[i++] = SYNC1;
-    pkt[i++] = seq_counter++;
+  pkt[i++] = SYNC0;
+  pkt[i++] = SYNC1;
+  pkt[i++] = seq_counter++;
 
-    memcpy(&pkt[i], slider_pressure, 32);
-    i += 32;
+  memcpy(&pkt[i], slider_pressure, 32);
+  i += 32;
 
-    memcpy(&pkt[i], air_beams, 6);
-    i += 6;
+  memcpy(&pkt[i], air_beams, 6);
+  i += 6;
 
-    pkt[i++] = test_btn;
-    pkt[i++] = service_btn;
+  pkt[i++] = test_btn;
+  pkt[i++] = service_btn;
 
-    // checksum
-    uint8_t checksum = 0;
-    for (size_t j = 2; j < i; j++) {
-        checksum ^= pkt[j];
-    }
-    pkt[i++] = checksum;
+  // checksum
+  uint8_t checksum = 0;
+  for (size_t j = 2; j < i; j++)
+  {
+    checksum ^= pkt[j];
+  }
+  pkt[i++] = checksum;
 
-    Serial.write(pkt, PACKET_SIZE);
+  Serial.write(pkt, PACKET_SIZE);
 }
 
-void setup() {
+void setup()
+{
 
-    USB.disconnect();
-    // Custom VID PID so chuniio.dll can find the device
-    USB.setVIDPID(PICO_USB_VID, PICO_USB_PID);
-    USB.setManufacturer("Jibhong");
-    USB.setProduct("ChuniSliderPico");
-    USB.connect();
+  USB.disconnect();
+  // Custom VID PID so chuniio.dll can find the device
+  USB.setVIDPID(PICO_USB_VID, PICO_USB_PID);
+  USB.setManufacturer("Jibhong");
+  USB.setProduct("ChuniSliderPico");
+  USB.connect();
 
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(16, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(16, INPUT_PULLUP);
+
+  initSlider();
 }
 
-void loop() {
-    scan_slider();
-    scan_air();
-    scan_buttons();
+void loop()
+{
+  scan_slider();
+  scan_air();
+  scan_buttons();
 
-    send_packet();
+  send_packet();
 
-    delay(1);
+  scanReboot();
+
+  delay(1);
 }
