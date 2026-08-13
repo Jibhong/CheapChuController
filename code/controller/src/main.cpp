3ac2939ecd9aaa60259b6d904b8139f436e73c5d
BuildTools <pisitjibhong@gmail.com>
2026-08-13 22:01:35 +0700

    Air work properly

--- code/controller/src/main.cpp
+++ code/controller/src/main.cpp
@@ -1,7 +1,5 @@
 
 
-
-
 // static void i2cScan() {
 //   Serial.println("Scanning Wire1 I2C bus...");
 //   uint8_t found = 0;
@@ -24,13 +22,11 @@
 //   Serial.println("Scan complete.");
 // }
 
-
 #include <Arduino.h>
 #include <USB.h>
 
 #include <Wire.h>
-extern "C"
-{
+extern "C" {
 #include "mpr121.h"
 }
 
@@ -46,7 +42,8 @@ extern "C"
 // ---------------------------------------------------------------------
 static constexpr uint8_t SYNC0 = 0xAA;
 static constexpr uint8_t SYNC1 = 0x55;
-static constexpr size_t PACKET_SIZE = 17; // 2 sync + 1 seq + 4 slider + 6 air + 3 btn + 1 checksum
+static constexpr size_t PACKET_SIZE =
+    17; // 2 sync + 1 seq + 4 slider + 6 air + 3 btn + 1 checksum
 
 static uint8_t seq_counter = 0;
 
@@ -57,61 +54,48 @@ static uint8_t test_btn = 0;
 static uint8_t service_btn = 0;
 static uint8_t coin_btn = 0;
 
-static void scanReboot()
-{
+static void scanReboot() {
   static bool isInit = 0;
-  if (!isInit)
-  {
+  if (!isInit) {
     pinMode(16, INPUT_PULLUP);
     isInit = 1;
   }
-  if (digitalRead(16) == LOW)
-  {
+  if (digitalRead(16) == LOW) {
     delay(20);
-    if (digitalRead(16) == LOW)
-    {
+    if (digitalRead(16) == LOW) {
       rp2040.reboot();
     }
   }
 }
 
-static void initSlider()
-{
+static void initSlider() {
   Wire1.setSDA(26);
   Wire1.setSCL(27);
   Wire1.begin();
 
-  if (!mpr121_init(MPR121_ADDR_A))
-  {
+  if (!mpr121_init(MPR121_ADDR_A)) {
     Serial.println("MPR121 A not found");
-    while (1)
-    {
+    while (1) {
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
       delay(200);
 
-      if (digitalRead(16) == LOW)
-      {
+      if (digitalRead(16) == LOW) {
         delay(20);
-        if (digitalRead(16) == LOW)
-        {
+        if (digitalRead(16) == LOW) {
           rp2040.reboot();
         }
       }
     }
   }
-  if (!mpr121_init(MPR121_ADDR_B))
-  {
+  if (!mpr121_init(MPR121_ADDR_B)) {
     Serial.println("MPR121 B not found");
-    while (1)
-    {
+    while (1) {
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
       delay(200);
 
-      if (digitalRead(16) == LOW)
-      {
+      if (digitalRead(16) == LOW) {
         delay(20);
-        if (digitalRead(16) == LOW)
-        {
+        if (digitalRead(16) == LOW) {
           rp2040.reboot();
         }
       }
@@ -120,100 +104,84 @@ static void initSlider()
 }
 
 // fill slider_bits[0..3] with 32 bits of pressure status.
-static void scan_slider()
-{
+static void scan_slider() {
   // Get the touch status of all 12 pins at once
   uint16_t touchedRegistersA = mpr121_touched(MPR121_ADDR_A);
   uint16_t touchedRegistersB = mpr121_touched(MPR121_ADDR_B);
 
-  for (int i = 0; i < 4; i++) slider_bits[i] = 0;
+  for (int i = 0; i < 4; i++)
+    slider_bits[i] = 0;
 
   // Loop through all 12 pins (0 to 11)
   for (uint8_t i = 0; i < 8; i++) {
     if (touchedRegistersA & (1 << i)) {
-      uint8_t idx = 31 - (i*2);
+      uint8_t idx = 31 - (i * 2);
       slider_bits[idx / 8] |= (1 << (idx % 8));
     }
   }
 
   for (uint8_t i = 4; i < 12; i++) {
     if (touchedRegistersB & (1 << i)) {
-      uint8_t idx = 31 - ((i - 4 + 8)*2);
+      uint8_t idx = 31 - ((i - 4 + 8) * 2);
       slider_bits[idx / 8] |= (1 << (idx % 8));
     }
   }
 }
 
 // fill air_beams[0..5] with 0/1.
-void scan_air()
-{
+void scan_air() {
   static uint32_t last_air_time = 0;
   static bool air_is_on = false;
+  static uint8_t current_sensor = 0;
 
-  if (!air_is_on)
-  {
-    digitalWrite(1, HIGH);
+  if (!air_is_on) {
+    digitalWrite(current_sensor, HIGH);
     last_air_time = micros();
     air_is_on = true;
     return; // Don't block, let the loop continue
   }
 
-  if (micros() - last_air_time < 1000)
-  {
+  if (micros() - last_air_time < 1000) {
     return; // Wait until 1000us has passed
   }
 
-  bool temp_is_air = (digitalRead(0) == LOW);
-  digitalWrite(1, LOW);
+  // 1 = hand not block, 0 = hand block
+  // Assuming the receiver pulls LOW when receiving IR light (not blocked).
+  // Swap 1 and 0 below if your sensor logic is inverted!
+  if (digitalRead(6 + current_sensor) == LOW) {
+    air_beams[current_sensor] = 1;
+  } else {
+    air_beams[current_sensor] = 0;
+  }
+
+  digitalWrite(current_sensor, LOW);
   air_is_on = false;
-
-  digitalWrite(LED_BUILTIN, temp_is_air ? HIGH : LOW);
-
-  static uint8_t temp_current_air = 0;
-
-  if (temp_is_air)
-  {
-    if (temp_current_air < 5) temp_current_air++;
-  }
-  else
-  {
-    if (temp_current_air > 0) temp_current_air--;
-  }
-
-  if (temp_current_air == 0 && !temp_is_air)
-  {
-    for (int i = 0; i < 6; i++) air_beams[i] = 0;
-  }
-  else
-  {
-    for (int i = 0; i < 6; i++)
-    {
-      air_beams[i] = (i <= temp_current_air) ? 1 : 0;
-    }
+  
+  current_sensor++;
+  if (current_sensor > 5) {
+    current_sensor = 0;
   }
 }
 
-void scan_buttons()
-{
-  if(digitalRead(10) == LOW)
+void scan_buttons() {
+  if (digitalRead(13) == LOW)
     test_btn = 1;
   else
     test_btn = 0;
 
-  if(digitalRead(11) == LOW)
+  if (digitalRead(14) == LOW)
     service_btn = 1;
   else
     service_btn = 0;
 
-  if(digitalRead(12) == LOW)
+  if (digitalRead(15) == LOW)
     coin_btn = 1;
   else
     coin_btn = 0;
 }
 
 // # Build and send one packet over USB serial.
-void send_packet()
-{
+void send_packet() {
   uint8_t pkt[PACKET_SIZE];
   size_t i = 0;
 
@@ -233,8 +201,7 @@ void send_packet()
 
   // checksum
   uint8_t checksum = 0;
-  for (size_t j = 2; j < i; j++)
-  {
+  for (size_t j = 2; j < i; j++) {
     checksum ^= pkt[j];
   }
   pkt[i++] = checksum;
@@ -242,8 +209,7 @@ void send_packet()
   Serial.write(pkt, PACKET_SIZE);
 }
 
-void setup()
-{
+void setup() {
 
   USB.disconnect();
   // Custom VID PID so chuniio.dll can find the device
@@ -257,18 +223,22 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(16, INPUT_PULLUP);
 
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, OUTPUT);
+  for (int i = 0; i <= 5; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
+  for (int i = 6; i <= 11; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
 
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
 
   initSlider();
 }
 
-void loop()
-{
+void loop() {
   scan_slider();
   scan_air();
   scan_buttons();
